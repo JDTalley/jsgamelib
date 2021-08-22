28b7e696e842460817dad37f428851a39ba242a9
Dakota <dakota.talley@gmail.com>
2021-08-22 18:13:02 -0400

    Added eslint changes. Fixed this binding.

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,105 +1,99 @@
 export default class Game {
-    constructor(GameCanvas) {
-        // Canvas Items
-        this.canvas = GameCanvas
-        this.gameWidth = canvas.width
-        this.gameHeight = canvas.height
+  constructor (GameCanvas) {
+    // Canvas Items
+    this.canvas = GameCanvas
+    this.gameWidth = this.canvas.width
+    this.gameHeight = this.canvas.height
 
-        // Timing Variables
-        this.deltaTime = .01
-        this.accumulator = 0
-        this.lastTick = performance.now()
+    // Timing Variables
+    this.deltaTime = 0.01
+    this.accumulator = 0
+    this.lastTick = performance.now()
 
-        // Capture input
-        this.keys = []
+    // Capture input
+    this.keys = []
 
-        // If keyUp, input is not active
-        window.onkeyup = (e) => {
-            keys[e.code] = false
-        }
-
-        // If keyDown, input is active
-        window.onkeydown = (e) => {
-            keys[e.code] = true
-            // PreventDefault to prevent page scrolling, etc.
-            e.preventDefault()
-        }
-
-
-        // Game Logic Variables
-        this.paused = false
+    // If keyUp, input is not active
+    window.onkeyup = (e) => {
+      this.keys[e.code] = false
     }
 
-    // startGame() initailizes game variables and starts the Game Loop
-    startGame() {
-        queueTick()
+    // If keyDown, input is active
+    window.onkeydown = (e) => {
+      this.keys[e.code] = true
+      // PreventDefault to prevent page scrolling, etc.
+      e.preventDefault()
     }
 
-    // queueTick() queues the next tick
-    queueTick() {
-        window.requestAnimationFrame(gameTick)
+    // Game Logic Variables
+    this.paused = false
+  }
+
+  // startGame() initailizes game variables and starts the Game Loop
+  startGame () {
+    window.requestAnimationFrame(this.gameTick.bind(this))
+  }
+
+  // gameTick() completes all actions for each tick and queues next tick
+  gameTick () {
+    if (!this.paused) {
+      // Update timing variables
+      this.updateTiming()
+
+      // Update Game Variables
+      this.updateGame()
+
+      // Checks for User input and makes changes
+      this.checkInput()
+
+      // Based on timing complete physics calculations
+      while (this.accumulator >= this.deltaTime) {
+        this.updatePhysics()
+        this.accumulator -= this.deltaTime
+      }
+
+      // Render graphics on screen
+      this.renderGraphics()
     }
 
-    // gameTick() completes all actions for each tick and queues next tick 
-    gameTick() {
-        if (!paused) {
-            // Update timing variables
-            updateTiming()
+    // Queue next tick
+    window.requestAnimationFrame(this.gameTick.bind(this))
+  }
 
-            // Update Game Variables
-            updateGame()
+  // updateTiming() updates timing variables to track tick calc times
+  updateTiming () {
+    // Calculate time since last tick
+    const currentTime = performance.now()
+    const elapsedTime = currentTime - this.lastTick
 
-            // Checks for User input and makes changes
-            checkInput()
+    // Make this tick the last tick
+    this.lastTick = currentTime
 
-            // Based on timing complete physics calculations
-            while (this.accumulator >= this.deltaTime) {
-                updatePhysics()
-                this.accumulator -= this.deltaTime
-            }
+    // Track accumulated time for physics calculations
+    this.accumulator += elapsedTime
+  }
 
-            // Render graphics on screen
-            renderGraphics()
-        }
+  // updateGame() updates Game Variables
+  updateGame () {
+    // Can add score, lives, etc.
+    // By default there are no items here
+  }
 
-        // Queue next tick
-        queueTick()
-    }
+  // checkInput() checks for inputs
+  checkInput () {
+    // Can add logic based on input here
+    // By default there are no items here
+  }
 
-    // updateTiming() updates timing variables to track tick calc times
-    updateTiming() {
-        // Calculate time since last tick
-        let currentTime = performance.now()
-        let elapsedTime = currentTime - this.lastTick
+  // updatePhysics() handles physics logic
+  updatePhysics () {
+    // Can add physics logic here
+    // By default there are no items here
+  }
 
-        // Make this tick the last tick
-        this.lastTick = currentTime
-
-        // Track accumulated time for physics calculations
-        this.accumulator += elapsedTime
-    }
-
-    // updateGame() updates Game Variables
-    updateGame() {
-        // Can add score, lives, etc.
-        // By default there are no items here
-    }
-
-    // checkInput() checks for inputs 
-    checkInput() {
-        // Can add logic based on input here
-        // By default there are no items here
-    }
-
-    // updatePhysics() handles physics logic
-    updatePhysics() {
-        // Can add physics logic here
-        // By default there are no items here
-    }
-
-    // renderGraphics() renders scene to canvas
-    renderGraphics() {
-        // Can add rendering here
-        // By default there are no items here
-    }
+  // renderGraphics() renders scene to canvas
+  renderGraphics () {
+    // Can add rendering here
+    // By default there are no items here
+  }
 }
